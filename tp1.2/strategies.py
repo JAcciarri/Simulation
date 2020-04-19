7fb73baa2820b70fe4078db2ec71840d2b9a1f37
Joshua <acciarrijoshua@gmail.com>
2020-04-19 20:13:01 -0300

    Minimal fixes. Bet-> unit value

--- tp1.2/strategies.py
+++ tp1.2/strategies.py
@@ -3,10 +3,9 @@
 
 import numpy as np
 
-
 def betToNumber(numbers, games, initCapital):
     capital = initCapital
-    betValue = 10
+    betValue = 1
     graph = [initCapital]
     chosen = int(input('Choose your number: '))
     for i in range(games):
@@ -20,7 +19,7 @@ def betToNumber(numbers, games, initCapital):
 
 def betToColor(numbers, games, initCapital):
     capital = initCapital
-    betValue = 10
+    betValue = 1
     graph = [initCapital]
     myColor = str(input('Now choose a color: (red) or (black): '))
     while(myColor != 'red' and myColor != 'black'):
@@ -36,20 +35,20 @@ def betToColor(numbers, games, initCapital):
     return graph
     
 def betAsSofovich(numbers, games, initCapital):
+    #This will probably be deleted
     capital = initCapital
-    betValue = 1000000
+    betValue = 100
     graph = [initCapital]
     notChosen_1 = int(input('Choose your NOT chosen number 1: '))
-    notChosen_2 = int(input('Choose your NOT chosen number 2: '))
-    for _ in range(2):
-        graph.clear()
-        graph.append(initCapital) 
-        capital = 100000000  
-        for i in range(games):
-            capital -= (betValue * 35)
-            rand = np.random.randint(0, len(numbers))
-            if(rand != notChosen_1 and rand != notChosen_2):
-                capital = capital + betValue * 36
-            graph.append(capital)
-    print()
+    notChosen_2 = int(input('Choose your NOT chosen number 2: ')) 
+    for i in range(games):
+        capital -= (betValue * 35)
+        rand = np.random.randint(0, len(numbers))
+        if(rand != notChosen_1 and rand != notChosen_2):
+            capital = capital + betValue * 36
+        graph.append(capital)
     return graph
+
+def betMartingala():
+    #Write code
+    return 
