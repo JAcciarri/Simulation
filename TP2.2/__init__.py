816b8592325a1d0876f81c9ca133bb02c5ecc254
Joshua <acciarrijoshua@gmail.com>
2020-06-05 11:03:23 -0300

    Improve structure. Gamma added

--- TP2.2/__init__.py
+++ TP2.2/__init__.py
@@ -12,27 +12,56 @@ def exponential(ex):
     x = -ex * ln(r)
     return x
 
-iterations = 1000
+def gamma(k, alpha):
+    tr = 1
+    for _ in range(k):
+        r = random()
+        tr = tr * r
+    x = -ln(tr) / alpha
+    return x
+
+# General parameters
+iterations = 500
+
+# Uniform parameters
 a = 5
 b = 19
-alpha = 3
+
+# Exponential parameters
+alpha_exp = 3
+
+# Gamma parameters
+k = 10
+alpha_gamma = 3
+
+
+
+# Main
 uniform_values = np.zeros(iterations)
 exp_values = np.zeros(iterations)
+gamma_values = np.zeros(iterations)
 
 for i in range(iterations):
     uniform_values[i] = uniform(a, b)
-    exp_values[i] = exponential(1/alpha)
+    exp_values[i] = exponential(1/alpha_exp)
+    gamma_values[i] = gamma(k, alpha_gamma)
 
-
+# Show
 print()
 print('-----UNIFORM VALUES------')
-print(uniform_values)
+#print(uniform_values)
 print('Mean value is', np.mean(uniform_values))
 print('and it is expected to be like', (a+b)/2)
 print()
 
 print('-----EXPONENTIAL VALUES------')
-print(exp_values)
+#print(exp_values)
 print('Mean value is', np.mean(exp_values))
-print('and it is expected to be like', 1/(alpha))
+print('and it is expected to be like', 1/alpha_exp)
+print()
+
+print('-----GAMMA VALUES------')
+print(gamma_values)
+print('Mean value is', np.mean(gamma_values))
+print('and it is expected to be like', k/alpha_gamma)
 print()
