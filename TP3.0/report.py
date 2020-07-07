e88a2e59f4d167adf540c0629464f199e3fd9f80
Nicolás Antonelli <niconelli2@gmail.com>
2020-07-07 10:25:16 -0300

    Combination of different processes in a single file

--- TP3.0/report.py
+++
@@ -1,7 +0,0 @@
-# Some info...
-def report(model):
-    print(f'\n Average delay in queue: {model["total_of_delays"] / model["num_customers_delayed"]}')
-    print(f'\n Average number of clients in queue: {model["area_num_in_queue"] / model["time"]}')
-    print(f'\n Server utilization: {model["area_server_status"] / model["time"]}')
-    print(f'\n Time simulation ended: {model["time"]}')
-
