47d9da169b80cf5b419affeafbac188fa6d7488a
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-07-08 16:46:41 -0300

    Refactor init code, mostly config selection

--- TP3.0/__init__.py
+++ TP3.0/__init__.py
@@ -33,41 +33,43 @@ n_runs = 10
 
 # Model Parameters Definition
 results = []
+n_delays = 1000
 configs = [
     {
         "arrival_rate": 0.5,
         "service_rate": 2,
-        "num_delays_required": 1000,
+        "num_delays_required": n_delays,
     },  # Arrival Rate = 0.25 * Service Rate
     {
         "arrival_rate": 1,
         "service_rate": 2,
-        "num_delays_required": 1000,
+        "num_delays_required": n_delays,
     },  # Arrival Rate = 0.5 * Service Rate
     {
         "arrival_rate": 1.5,
         "service_rate": 2,
-        "num_delays_required": 1000,
+        "num_delays_required": n_delays,
     },  # Arrival Rate = 0.75 * Service Rate
     {
         "arrival_rate": 2,
         "service_rate": 2,
-        "num_delays_required": 1000,
+        "num_delays_required": n_delays,
     },  # Arrival Rate = 1 * Service Rate
     {
         "arrival_rate": 2.5,
         "service_rate": 2,
-        "num_delays_required": 1000,
+        "num_delays_required": n_delays,
     },  # Arrival Rate = 1.25 * Service Rate
 ]
+current_config = configs[3]
 
 # Main
 if __name__ == "__main__":
     for i in range(n_runs):
         print("\nModel " + str(i + 1) + ":")
-        result = run_queue_simulation(configs[3])
+        result = run_queue_simulation(current_config)
         results.append(result)
-        expected = get_expected_values(configs[3])
+        expected = get_expected_values(current_config)
         # Other Runs with other configs...
 
     # print((i + 1), "iterations results:", results)
