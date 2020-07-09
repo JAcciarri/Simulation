b33be3eca42b038703646bceab39a6e89fc78f26
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-07-09 05:03:56 -0300

    Add bar plot for expected and observed n clients in queue probability

--- TP3.0/plots.py
+++ TP3.0/plots.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Save any plot
@@ -41,7 +42,7 @@ def plot_one(measures_from_multiple_runs, expected_value, title, x_label, y_labe
 # Specification of all the graphs to plot
 def plot_results(results, expected, save):
     # Average quantity of costumers in queue
-    plot_one(
+    """plot_one(
         [result["avg_num_in_queue"] for result in results],
         expected_value=expected["Lq"],
         title="Average quantity of costumers in queue",
@@ -93,7 +94,15 @@ def plot_results(results, expected, save):
         y_label="U(n)",
         save=save,
         name="server_utilization",
-    )
+    )"""
+
+    expected_pn_shortened = expected["Pn"][:20]
+    plt.bar(np.arange(len(expected_pn_shortened)), expected_pn_shortened, width=0.05)
+
+    average_observed_pn = list(
+        map(np.mean, zip(*[result["n_clients_in_queue_probability_array"] for result in results]))
+    )[:20]
+    plt.bar(np.arange(len(average_observed_pn)), average_observed_pn, width=0.05)
 
     # Show all the plots
     plt.show()
