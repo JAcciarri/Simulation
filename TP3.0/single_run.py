41d910ac533b531de6865f5a165ed03c25238774
Nicolás Antonelli <niconelli2@gmail.com>
2020-07-10 16:20:15 -0300

    Added Relation Analytic/Sumulated on Console

--- TP3.0/single_run.py
+++ TP3.0/single_run.py
@@ -48,65 +48,47 @@ def values_comparison(results, expected):
     print("μ  (Service rate):", expected["Mu"])
 
     if expected["Lq"] is not None:
+        # Analytic Performance Measures Print
         print("\nAnalytic Performance Measures")
-        print("ρ  (Server utilization):", expected["Rho"])
-        print("Lq (Average quantity of costumers in queue):", expected["Lq"])
-        print("Wq (Average delay time in queue):", expected["Wq"])
-        print("L  (Average quantity of costumers in the system):", expected["L"])
-        print("W  (Average delay time in the system):", expected["W"])
-        print("Pn (N customers in queue probability), (0 ≤ N < 20):")
-        print(np.round(np.array(expected["Pn"][:20]), 3))
-        print("Pd (Denial of service probability):", expected["Pd"])
+        print("ρ  (Server utilization):", np.round(expected["Rho"], 6))
+        print("Lq (Average quantity of costumers in queue):", np.round(expected["Lq"], 6))
+        print("Wq (Average delay time in queue):", np.round(expected["Wq"], 6))
+        print("L  (Average quantity of costumers in the system):", np.round(expected["L"], 6))
+        print("W  (Average delay time in the system):", np.round(expected["W"], 6))
+        print("Pn (N customers in system probability), (0 ≤ N < 20):")
+        print(np.round(np.array(expected["Pn"][:20]), 6))
+        print("Pd (Denial of service probability):", np.round(expected["Pd"], 6))
 
-        print("\nSimulation Performance Measures")
-        print(
-            "ρ  (Server utilization):",
-            np.round(
-                np.mean([list(result["server_utilization"].values())[-1] for result in results]), 3
-            ),
-        )
-        print(
-            "Lq (Average quantity of costumers in queue):",
-            np.round(
-                np.mean([list(result["avg_num_in_queue"].values())[-1] for result in results]), 3
-            ),
-        )
-        print(
-            "Wq (Average delay time in queue):",
-            np.round(
-                np.mean([list(result["avg_delay_in_queue"].values())[-1] for result in results]), 3
-            ),
-        )
-        print(
-            "L  (Average quantity of costumers in the system):",
-            np.round(
-                np.mean([list(result["avg_num_in_system"].values())[-1] for result in results]), 3
-            ),
-        )
-        print(
-            "W  (Average delay time in the system):",
-            np.round(
-                np.mean([list(result["avg_delay_in_system"].values())[-1] for result in results]), 3
-            ),
-        )
-        print("Pn (N customers in queue probability), (0 ≤ N < 20):")
-        print(
-            np.round(
-                np.mean(
-                    [result["n_clients_in_queue_probability_array"][:20] for result in results],
-                    axis=0,
-                ),
-                3,
-            )
-        )
-        print(
-            "Pd (Denial of service probability):",
-            np.round(
-                np.mean([result["client_not_getting_service_probability"] for result in results]), 3
-            ),
-        )
+        print("\nSimulation Performance Measures (Runs' Average)")
+        # Average Simulation Performance Measures Calculation
+        Rho = np.mean([list(result["server_utilization"].values())[-1] for result in results])
+        Lq = np.mean([list(result["avg_num_in_queue"].values())[-1] for result in results])
+        Wq = np.mean([list(result["avg_delay_in_queue"].values())[-1] for result in results])
+        L = np.mean([list(result["avg_num_in_system"].values())[-1] for result in results])
+        W = np.mean([list(result["avg_delay_in_system"].values())[-1] for result in results])
+        Pn = np.mean([result["n_clients_in_system_probability_array"][:20] for result in results], axis=0)
+        Pd = np.mean([result["client_not_getting_service_probability"] for result in results])
+        # Average Simulation Performance Measures Print
+        print("ρ  (Server utilization):", np.round(Rho, 6))
+        print("Lq (Average quantity of costumers in queue):", np.round(Lq, 6))
+        print("Wq (Average delay time in queue):", np.round(Wq, 6))
+        print("L  (Average quantity of costumers in the system):", np.round(L, 6))
+        print("W  (Average delay time in the system):", np.round(W, 6))
+        print("Pn (N customers in system probability), (0 ≤ N < 20):")
+        print(np.round(np.array(Pn), 6))
+        print("Pd (Denial of service probability):", np.round(Pd, 6))
+
+        # Expected VS Simulated Comparison (Main Parameters Only)
+        print("\nRelation Between Analytics and Simulated Main Values")
+        print("ρ  = ", np.round(expected["Rho"] / Rho, 6))
+        print("Lq = ", np.round(expected["Lq"] / Lq, 6))
+        print("Wq = ", np.round(expected["Wq"] / Wq, 6))
+        print("L  = ", np.round(expected["L"] / L, 6))
+        print("W  = ", np.round(expected["W"] / W, 6))
         print()
+
     else:
+        # λ ≥ μ
         print("\nPerformance Measures")
         print("λ ≥ μ, the queue grows infinitely")
         print("ρ  (Server utilization):", expected["Rho"])
